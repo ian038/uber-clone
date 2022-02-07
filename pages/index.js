c5830ea2924b5a682e83d89c33bb3736f4e6b3af
ian038 <ianphua3@gmail.com>
2022-02-07 16:52:24 -0500

    Added styling for home

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import tw from "tailwind-styled-components";
 import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter()
+  
   return (
     <Wrapper>
       <ActionItems>
